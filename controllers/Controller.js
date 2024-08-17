ff22482377e24a5decff1fba3c9b0d7f0568517c
Harshit <mishraharshit111@gmail.com>
2024-08-17 17:23:47 +0530

    Added feature to update existing Rule

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -4,25 +4,47 @@ const astUtils = require("../utils/ast");
 
 exports.createRule = async (req, res) => {
   try {
-    const { ruleName, ruleString } = req.body;
+    const { ruleName, ruleString, update = false } = req.body;
     if (!ruleName || !ruleString) {
       return res
         .status(400)
         .json({ error: "ruleName and ruleString are required" });
     }
 
-    // Check if a rule with the same name already exists
-    const existingRule = await Rule.findOne({ ruleName });
-    if (existingRule) {
-      return res.status(400).json({ error: "Rule name already exists" });
+    const ruleAST = tokenizeRuleString(ruleString);
+
+    let rule;
+    if (update) {
+      // Find and update existing rule
+      rule = await Rule.findOneAndUpdate(
+        { ruleName },
+        { ruleAST },
+        { new: true }
+      );
+      if (!rule) {
+        return res.status(404).json({ error: "Rule not found for update" });
+      }
+    } else {
+      // Check if the rule already exists
+      const existingRule = await Rule.findOne({ ruleName });
+      if (existingRule) {
+        return res
+          .status(400)
+          .json({ error: "Rule with this name already exists" });
+      }
+
+      rule = new Rule({ ruleName, ruleAST });
+      await rule.save();
     }
 
-    const rootNode = tokenizeRuleString(ruleString);
-    const rule = new Rule({ ruleName, ruleAST: rootNode });
-    await rule.save();
-    res.status(201).json({ message: "Rule created successfully", rule });
+    res.json({
+      message: update
+        ? "Rule updated successfully"
+        : "Rule created successfully",
+      ruleAST,
+    });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(400).json({ error: error.message });
   }
 };
 
@@ -55,12 +77,10 @@ exports.combineRules = async (req, res) => {
 
     await combinedRule.save();
 
-    res
-      .status(201)
-      .json({
-        ruleName: `${rule1Name}_${rule2Name}_${op}`,
-        ruleAST: combinedAST,
-      });
+    res.status(201).json({
+      ruleName: `${rule1Name}_${rule2Name}_${op}`,
+      ruleAST: combinedAST,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "An error occurred while combining rules" });
